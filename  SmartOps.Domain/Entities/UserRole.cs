353de8c1211890c6b3ac24e38e62510592f91fed
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-09-06 11:19:52 +0530

    update

---
+++  SmartOps.Domain/Entities/UserRole.cs
@@ -0,0 +1,29 @@
+﻿namespace SmartOps.Domain.Entities;
+
+public class UserRole
+{
+    public Guid UserId { get; private set; }
+    public Guid RoleId { get; private set; }
+
+    private UserRole()
+    {
+    }
+
+    public UserRole(
+        Guid userId,
+        Guid roleId)
+    {
+        if (userId == Guid.Empty)
+            throw new ArgumentException(
+                "User is required.",
+                nameof(userId));
+
+        if (roleId == Guid.Empty)
+            throw new ArgumentException(
+                "Role is required.",
+                nameof(roleId));
+
+        UserId = userId;
+        RoleId = roleId;
+    }
+}
