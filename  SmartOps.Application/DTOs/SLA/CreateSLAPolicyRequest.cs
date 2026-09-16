d7da242a3e72c5f045dcb3e7dbb626b07854b00d
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-09-16 19:32:02 +0530

    update

---
+++  SmartOps.Application/DTOs/SLA/CreateSLAPolicyRequest.cs
@@ -0,0 +1,14 @@
+﻿using SmartOps.Domain.Enums;
+
+namespace SmartOps.Application.DTOs.SLA;
+
+public class CreateSLAPolicyRequest
+{
+    public string Name { get; set; } = string.Empty;
+
+    public Priority Priority { get; set; }
+
+    public int ResponseTimeMinutes { get; set; }
+
+    public int ResolutionTimeMinutes { get; set; }
+}
