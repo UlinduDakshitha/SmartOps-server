b01541db0f2a39e8d902ec98289fcdc82b0a8e6c
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-09-21 20:26:56 +0530

    update

---
+++ SmartOps.API/Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SmartOps.Application.DTOs.Users;
+using SmartOps.Application.Interfaces.Services;
+
+namespace SmartOps.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await _userService.GetAllAsync();
+
+        return Ok(result);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var result = await _userService.GetByIdAsync(id);
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(
+        CreateUserRequest request)
+    {
+        var result = await _userService.CreateAsync(request);
+
+        return Ok(result);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> Update(
+        Guid id,
+        UpdateUserRequest request)
+    {
+        var result = await _userService.UpdateAsync(
+            id,
+            request);
+
+        return Ok(result);
+    }
+
+    [HttpPatch("{id:guid}/activate")]
+    public async Task<IActionResult> Activate(Guid id)
+    {
+        await _userService.ActivateAsync(id);
+
+        return Ok(new
+        {
+            Message = "User activated successfully."
+        });
+    }
+
+    [HttpPatch("{id:guid}/deactivate")]
+    public async Task<IActionResult> Deactivate(Guid id)
+    {
+        await _userService.DeactivateAsync(id);
+
+        return Ok(new
+        {
+            Message = "User deactivated successfully."
+        });
+    }
+}
