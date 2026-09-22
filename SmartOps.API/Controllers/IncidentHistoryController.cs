6d5b4e9b1209533e85070a53da1fc0de4cf9df87
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-09-22 20:23:03 +0530

    update

---
+++ SmartOps.API/Controllers/IncidentHistoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SmartOps.Application.Interfaces.Services;
+
+namespace SmartOps.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class IncidentHistoryController : ControllerBase
+{
+    private readonly IIncidentHistoryService _historyService;
+
+    public IncidentHistoryController(
+        IIncidentHistoryService historyService)
+    {
+        _historyService = historyService;
+    }
+
+    [HttpGet("incident/{incidentId:guid}")]
+    public async Task<IActionResult> GetByIncidentId(
+        Guid incidentId)
+    {
+        var history = await _historyService
+            .GetByIncidentIdAsync(incidentId);
+
+        return Ok(history);
+    }
+}
---
+++ SmartOps.Application/Services/IncidentHistoryService.cs
@@ -0,0 +1,31 @@
+﻿using SmartOps.Application.Interfaces.Repositories;
+using SmartOps.Application.Interfaces.Services;
+using SmartOps.Domain.Entities;
+
+namespace SmartOps.Application.Services;
+
+public class IncidentHistoryService : IIncidentHistoryService
+{
+    private readonly IIncidentHistoryRepository _historyRepository;
+    private readonly IIncidentRepository _incidentRepository;
+
+    public IncidentHistoryService(
+        IIncidentHistoryRepository historyRepository,
+        IIncidentRepository incidentRepository)
+    {
+        _historyRepository = historyRepository;
+        _incidentRepository = incidentRepository;
+    }
+
+    public async Task<List<IncidentHistory>> GetByIncidentIdAsync(
+        Guid incidentId)
+    {
+        var incident = await _incidentRepository.GetByIdAsync(incidentId);
+
+        if (incident is null)
+            throw new KeyNotFoundException("Incident not found.");
+
+        return await _historyRepository
+            .GetByIncidentIdAsync(incidentId);
+    }
+}
